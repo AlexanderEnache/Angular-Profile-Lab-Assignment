249619bc18992609d77df2a738de63a16007eaa0
Alex Enache <ragnarxon@gmail.com>
2019-02-15 08:40:24 -0500

    Made default landing page

--- app.config.js
+++ app.config.js
@@ -3,6 +3,7 @@
 angular.module("ProfileApp").config(["$routeProvider", function($routeProvider){
 	
 	$routeProvider
+	.when("/", {template: "<page></page>"})
 	.when("/page", {template: "<page></page>"})
 	.when("/edit", {template: "<edit></edit>"})
 	.otherwise({redirectTo: "/"});
